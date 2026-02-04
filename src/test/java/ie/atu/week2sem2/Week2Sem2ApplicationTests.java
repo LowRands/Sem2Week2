86145d475c4697af12d1af91b979e824e0bfdd87
EthanLourens-STUDENT <G00467054@atu.ie>
2026-02-04 16:39:18 +0000

    Created Project

--- src/test/java/ie/atu/week2sem2/Week2Sem2ApplicationTests.java
+++ src/test/java/ie/atu/week2sem2/Week2Sem2ApplicationTests.java
@@ -1,13 +1,16 @@
 package ie.atu.week2sem2;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class Week2Sem2ApplicationTests {
-
     @Test
     void contextLoads() {
-    }
 
+    }
 }
